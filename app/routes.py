dc6d85d2d5b882a4c94b21bc665edb15e9ffbc74
laxier <hash0xcb8c@yandex.ru>
2024-04-13 01:33:51 +0300

    finished the login-registration process

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.forms import RegistrationForm
 from flask import request
 from flask_login import current_user, login_user
 from flask_login import logout_user
@@ -43,6 +44,19 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 @app.route('/logout')
 def logout():
     logout_user()
