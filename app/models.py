485b62c6b09c58c18447cbeedf9dd6a83a1f428f
laxier <hash0xcb8c@yandex.ru>
2024-04-13 01:16:02 +0300

    conditional login and logout links

--- app/models.py
+++ app/models.py
@@ -1,11 +1,14 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from app import login
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -29,3 +32,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
