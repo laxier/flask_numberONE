3b4185a3902ed3d7bcfef52b83647ee4f44e481d
laxier <hash0xcb8c@yandex.ru>
2024-04-14 01:32:44 +0300

    made a good-looking header, later will continue working on the post publication

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+bootstrap = Bootstrap5(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
